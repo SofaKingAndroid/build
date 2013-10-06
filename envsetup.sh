1b9ca2faaae97ac6bc4ee588f3048d6278f4a0e6
jamieg71 <j.gowans71@gmail.com>
2013-10-06 09:09:35 -0500

    Initial for Saber/Linaro
    Change-Id: I4843aae1c9002d2d74908dffd862793767c4e7ef

--- envsetup.sh
+++ envsetup.sh
@@ -142,7 +142,7 @@ function setpaths()
     case $ARCH in
         x86) toolchaindir=x86/i686-linux-android-$targetgccversion/bin
             ;;
-        arm) toolchaindir=arm/arm-linux-androideabi-$targetgccversion/bin
+        arm) toolchaindir=arm/androideabi-4.7/bin
             ;;
         mips) toolchaindir=mips/mipsel-linux-android-$targetgccversion/bin
             ;;
@@ -158,8 +158,8 @@ function setpaths()
     unset ARM_EABI_TOOLCHAIN ARM_EABI_TOOLCHAIN_PATH
     case $ARCH in
         arm)
-            toolchaindir=arm/arm-eabi-$targetgccversion/bin
-            if [ -d "$gccprebuiltdir/$toolchaindir" ]; then
+          toolchaindir=arm/arm-eabi-4.7/bin
+          if [ -d "$gccprebuiltdir/$toolchaindir" ]; then
                  export ARM_EABI_TOOLCHAIN="$gccprebuiltdir/$toolchaindir"
                  ARM_EABI_TOOLCHAIN_PATH=":$gccprebuiltdir/$toolchaindir"
             fi

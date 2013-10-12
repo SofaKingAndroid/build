1c35fc013fd3941687cf057dfaf41a519220e0a0
GruesomeWolf <boober19841126@gmail.com>
2013-10-12 14:24:04 -0400

    will revert after we fix errors

--- envsetup.sh
+++ envsetup.sh
@@ -142,7 +142,7 @@ function setpaths()
     case $ARCH in
         x86) toolchaindir=x86/i686-linux-android-$targetgccversion/bin
             ;;
-        arm) toolchaindir=arm/androideabi-4.7/bin
+        arm) toolchaindir=arm/android-toolchain-eabi/bin
             ;;
         mips) toolchaindir=mips/mipsel-linux-android-$targetgccversion/bin
             ;;
@@ -158,7 +158,7 @@ function setpaths()
     unset ARM_EABI_TOOLCHAIN ARM_EABI_TOOLCHAIN_PATH
     case $ARCH in
         arm)
-          toolchaindir=arm/arm-eabi-4.7/bin
+          toolchaindir=arm/arm-eabi-$targetgccversion/bin
           if [ -d "$gccprebuiltdir/$toolchaindir" ]; then
                  export ARM_EABI_TOOLCHAIN="$gccprebuiltdir/$toolchaindir"
                  ARM_EABI_TOOLCHAIN_PATH=":$gccprebuiltdir/$toolchaindir"
